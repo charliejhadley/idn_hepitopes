987cec0b8b6494255aaaaa5c7961c8ec5313114a
martinjhnhadley <martinjohnhadley@gmail.com>
2017-11-01 10:52:52 +0000

    moved code to fix bug with navbarPage
    In past versions of Shiny it was possible to put whatever you liked in the first argument of navbarPage but it's changed so you can only put the "brand" in.

--- ui.R
+++ ui.R
@@ -14,17 +14,17 @@ library(htmltools)
 library(shinyBS)
 
 shinyUI(navbarPage(
-  tags$head(includeScript("google-analytics.js"),
-            tags$style(type="text/css",".navbar .container-fluid, .navbar-collapse {
+  "",
+    tabPanel(
+      "Hepitopes Database",
+      fluidPage(
+        tags$head(includeScript("google-analytics.js"),
+                  tags$style(type="text/css",".navbar .container-fluid, .navbar-collapse {
     padding-left:0;
                        }
                        .navbar-collapse.in {
                        padding-left:30px;
                        }")),
-  "",
-    tabPanel(
-      "Hepitopes Database",
-      fluidPage(
         tags$style("overflow-y:scroll; max-height: 100%"),
         wellPanel(p("To use the database, optimise your view by selecting or deleting columns from the 'columns to show' toolbar at the top of the page. Use the up/down arrows at the right of each column header to sort the data, or type your free-text search selection into the box beneath the column header."),"NS=not specified, NA=not available."),
         uiOutput("HEPITOPES_selected_cols_UI"),
